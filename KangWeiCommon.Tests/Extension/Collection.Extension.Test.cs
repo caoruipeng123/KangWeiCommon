9431a4963fc0081dac62ebda123901f8418fecd7
曹瑞鹏 <caoruipeng@outlook.com>
2021-07-03 21:14:54 +0800

    新增集合相关扩展方法。

---
+++ KangWeiCommon.Tests/Extension/Collection.Extension.Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KangWeiCommon.Tests.Extension
+{
+    /// <summary>
+    /// 集合相关扩展方法
+    /// </summary>
+    [TestClass]
+    public class CollectionExtensionTest
+    {
+        /// <summary>
+        /// 是否有长度
+        /// </summary>
+        [TestMethod]
+        public void HasLength()
+        {
+            string[] array = null;
+            Assert.AreEqual(array.HasLength(), false);
+
+            array = new string[0];
+            Assert.AreEqual(array.HasLength(), false);
+
+            array = new string[1] { "111" };
+            Assert.AreEqual(array.HasLength(), true);
+        }
+        /// <summary>
+        /// 是否没有长度
+        /// </summary>
+        [TestMethod]
+        public void HasNotLength()
+        {
+            string[] array = null;
+            Assert.AreEqual(array.HasNotLength(), true);
+
+            array = new string[0];
+            Assert.AreEqual(array.HasNotLength(), true);
+
+            array = new string[1] { "111" };
+            Assert.AreEqual(array.HasNotLength(), false);
+        }
+    }
+}
---
+++ KangWeiCommon/Extension/Collection.Extension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KangWeiCommon
+{
+    /// <summary>
+    /// 系统类的扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 判断一个集合中是否有元素。有返回true
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static bool HasLength(this System.Collections.IEnumerable collection)
+        {
+            if (collection == null)
+            {
+                return false;
+            }
+            var a = collection.GetEnumerator();
+            return a.MoveNext();
+        }
+        /// <summary>
+        /// 判断一个集合中是否 没有元素。没有返回true
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static bool HasNotLength<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                return true;
+            }
+            var a = collection.GetEnumerator();
+            return !a.MoveNext();
+        }
+    }
+}
