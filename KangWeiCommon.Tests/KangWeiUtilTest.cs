f1d9e8d6d1ba0b41705fb541872fa79a8ead88ab
caoruipeng <caoruipeng@16.com>
2021-07-30 10:05:06 +0800

    新增IP地址和Uint类型相互转换。

---
+++ KangWeiCommon.Tests/KangWeiUtilTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KangWeiCommon.Tests
+{
+    [TestClass]
+    public class KangWeiUtilTest
+    {
+        /// <summary>
+        /// IP地址和Uint类型相互转换
+        /// </summary>
+        [TestMethod]
+        public void IpUIntTest()
+        {
+            Random random = new Random();
+            for(int i = 0; i < 1000000; i++)
+            {
+                string ip = $"{random.Next(1,215)}.{random.Next(1, 215)}.{random.Next(1, 215)}.{random.Next(1, 215)}";
+                uint ipNumber = KangWeiUtil.IpToUInt(ip);
+                string ips = KangWeiUtil.UIntToIp(ipNumber);
+                Assert.AreEqual(ip, ips);
+            }
+
+        }
+    }
+}
---
+++ KangWeiCommon/KangWeiUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KangWeiCommon
+{
+    public class KangWeiUtil
+    {
+        /// <summary>
+        /// ip地址转换为UInt类型
+        /// </summary>
+        /// <param name="ip">ip地址</param>
+        /// <returns></returns>
+        /// <example>192.168.20.46</example>
+        public static uint IpToUInt(string ip)
+        {
+            string[] array = ip.Split('.');
+            uint[] arr = new uint[4];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[i] = Convert.ToByte(array[i]);
+            }
+            uint ret = 0;
+            ret = ((arr[0] & 0xff) | ret) << 8;
+            ret = ((arr[1] & 0xff) | ret) << 8;
+            ret = ((arr[2] & 0xff) | ret) << 8;
+            ret = ((arr[3] & 0xff) | ret);
+            return ret;
+        }
+        /// <summary>
+        /// int转换为ip地址
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string UIntToIp(uint number)
+        {
+            return new StringBuilder()
+                   .Append((number >> 24) & 0xff)
+                   .Append('.')
+                   .Append((number >> 16) & 0xff)
+                   .Append('.')
+                   .Append((number >> 8) & 0xff)
+                   .Append('.')
+                   .Append((number & 0xff))
+                   .ToString();
+        }
+    }
+}
