ed3129a2eea0572d8304ed3219bca9de1696031c
caoruipeng <caoruipeng123@163.com>
2021-10-08 17:59:56 +0800

    新建KangWeiCommonRef项目，用于nuget引入KangWeiCommon实地测试。

---
+++ KangWeiCommonRef/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KangWeiCommon;
+namespace KangWeiCommonRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IdWorker worker = new IdWorker(1, 10);
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(worker.NextId());
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
