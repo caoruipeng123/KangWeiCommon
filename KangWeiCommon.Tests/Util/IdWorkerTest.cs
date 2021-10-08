b14bbc958944b36a4f93cd692f042728bde99d69
caoruipeng <caoruipeng123@163.com>
2021-10-08 15:38:10 +0800

    1、新增雪花算法自增生成器IdWorker类。 2、新增单例模式父类Singleton类，其他单例类可以直接继承该类，无需自己实现单例模式 。

---
+++ KangWeiCommon.Tests/Util/IdWorkerTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KangWeiCommon.Tests.Util
+{
+    /// <summary>
+    /// 雪花算法自增生成器测试
+    /// </summary>
+    [TestClass]
+    public class IdWorkerTest
+    {
+        /// <summary>
+        /// 是否有长度
+        /// </summary>
+        [TestMethod]
+        public void IdWorker()
+        {
+            IdWorker worker = new IdWorker(11, 11);
+            int length = 10000;
+            List<long> list = new List<long>(length);
+            while (list.Count <= length)
+            {
+                long a = worker.NextId();
+                list.Add(a);
+            }
+            list.Sort();
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(list[i]);
+                Assert.IsTrue(list[i] < list[i + 1]);
+            }
+        }
+    }
+}
---
+++ KangWeiCommon/DesignPattern/Singleton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KangWeiCommon.DesignPattern
+{
+    /// <summary>
+    /// 单例模式实现
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Singleton<T> where T : class
+    {
+        /// <summary>
+        /// 当前类的唯一实例
+        /// </summary>
+        private static T _instance;
+        /// <summary>
+        /// 全局锁 
+        /// </summary>
+        private static readonly object syslock = new object();
+        /// <summary>
+        /// 无参构造函数
+        /// </summary>
+        protected Singleton()
+        {
+        }
+        /// <summary>
+        /// 当前类的唯一实例
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syslock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = Activator.CreateInstance<T>();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
